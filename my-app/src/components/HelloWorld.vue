2328855f45b752c21764b7c8c84ce494eb4c1325
aziya-500apps <aziya.shaik@500apps.com>
2022-03-23 11:19:08 +0530

    comitted

--- my-app/src/components/HelloWorld.vue
+++ my-app/src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="hello">
+    <one></one>
+    <secondName></secondName>
     <h1>Hello World</h1>
     <h2>this is string</h2>
    
@@ -8,8 +10,15 @@
 </template>
 
 <script>
+import one from '../first.vue'
+import secondName from './second.vue'
 export default {
   name: 'HelloWorld',
+   
+  components:{
+    one,
+    secondName,
+  },
   props: {
     msg: String
   }
