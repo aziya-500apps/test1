c0304e3d523a0a9b79debafe7057478cc604744d
aziya-500apps <aziya.shaik@500apps.com>
2022-03-23 19:13:21 +0530

    firstcommit

---
+++ proj1/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+   
+   <pc1 msg="Hello" ref="ref" name="Aziya" number=123456789></pc1>
+   <!-- <pc2 :msg="msg" :name="name" :number="number"></pc2>
+   <pc3 ref="msg" >{{msg}}</pc3> -->
+   <button @click="display()">submit</button>
+    
+  </div>
+</template>
+
+<script>
+import pc1 from "@/components/pc1.vue"
+// import pc2 from "@/components/pc2.vue"
+// import pc3 from "@/components/pc3.vue"
+export default {
+  name: 'HelloWorld',
+  components:{
+    pc1,
+    // pc2,
+    // pc3,
+  },
+  data(){
+    return {
+      msg:"hello",
+      name:"Neha",
+      number:11111111,
+    };
+  },
+    methods :{
+       display(){
+         this.$refs.ref.f1();
+       }
+    }
+}
+</script>
+
---
+++ proj1/src/components/pc1.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h4>parent child by using props concept</h4>
+        <h4>msg:{{ msg }}</h4>
+        <h4>name:{{ name }}</h4>
+        <h4>number:{{ number }}</h4>
+    </div>
+</template>
+<script>
+export default {
+    name:"pcFile1",
+    props: {
+    msg: String,
+    name:String,
+    number:Number,
+  },
+    
+    methods: {
+     f1(){
+         alert("|hii!")
+     }
+    }
+}
+</script>
